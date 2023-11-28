7914e0e9eb5dc7a2dd98eeef9766d693f26df02d
Thouhid98 <thouhidhussain98@gmail.com>
2023-11-29 01:14:43 +0600

    Admin verify done

--- index.js
+++ index.js
@@ -31,6 +31,38 @@ async function run() {
         // User Related Api 
         const userCollection = client.db('Medicaldb').collection('users')
 
+        // MiddleWare for verify users token 
+        const verifyToken = (req, res, next) => {
+            // console.log('Inside middleware', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'Forbidden Acces' })
+            }
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: "Forbidden Access" })
+                }
+                req.decoded = decoded
+                next()
+            })
+        }
+        
+        // Checking User Admin or Not 
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'Unauthorized Access' })
+            }
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            let admin = false
+            if (user) {
+                admin = user?.role === 'admin'
+            }
+            res.send({ admin })
+        })
+
+
         // Jwt Releted APis 
         app.post('/jwt', async (req, res) => {
             const user = req.body;
@@ -38,7 +70,7 @@ async function run() {
             res.send({ token })
         })
 
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyToken, async (req, res) => {
             const result = await userCollection.find().toArray()
             res.send(result)
         })
